28d1c91476fc53c2c1a02dbae6021e147011e8d1
RiccardoParrino <riccardoparrino1@gmail.com>
2026-03-30 22:25:54 +0200

    add function interfaces

--- demo/src/main/java/io/functional/programming/function/Function2.java
+++ demo/src/main/java/io/functional/programming/function/Function2.java
@@ -3,4 +3,18 @@ package io.functional.programming.function;
 @FunctionalInterface
 public interface Function2<T1, T2, R> {
     R apply(T1 t1, T2 t2);
+
+    default Function1<T2,R> apply(T1 t1) {
+        return curried()
+                .apply(t1);
+    }
+
+    default Function1<T1, Function1<T2,R>> curried() {
+        return t1 -> {
+            return t2 -> {
+                return apply(t1, t2);
+            };
+        };
+    }
+
 }
--- demo/src/main/java/io/functional/programming/function/Function3.java
+++ demo/src/main/java/io/functional/programming/function/Function3.java
@@ -3,4 +3,23 @@ package io.functional.programming.function;
 @FunctionalInterface
 public interface Function3<T1, T2, T3, R> {
     R apply(T1 t1, T2 t2, T3 t3);
+
+    default Function1<T2, Function1<T3,R>> apply(T1 t1) {
+        return curried()
+                .apply(t1);
+    }
+
+    default  Function1<T3, R> apply(T1 t1, T2 t2) {
+        return this.apply(t1).apply(t2);
+    }
+
+    default Function1<T1, Function1<T2, Function1<T3,R>>> curried() {
+        return t1 -> {
+            return t2 -> {
+                return t3 -> {
+                    return this.apply(t1,t2,t3);
+                };
+            };
+        };
+    }
 }
